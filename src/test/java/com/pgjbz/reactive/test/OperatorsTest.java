4755d389e561ce045e96cf7b88d2cc00408d2957
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2021-07-15 19:51:58 -0300

    fromCallable

--- src/test/java/com/pgjbz/reactive/test/OperatorsTest.java
+++ src/test/java/com/pgjbz/reactive/test/OperatorsTest.java
@@ -1,10 +1,16 @@
 package com.pgjbz.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 @Slf4j
 class OperatorsTest {
@@ -130,6 +136,26 @@ class OperatorsTest {
         StepVerifier.create(flux)
                 .expectSubscription()
                 .expectNext(1,2,3,4)
+                .verifyComplete();
+    }
+
+    @Test
+    void subscribeOnIO() throws Exception{
+        Mono<List<String>> list = Mono.fromCallable(() -> Files.readAllLines(Path.of("text-file")))
+                .log()
+                .subscribeOn(Schedulers.boundedElastic());
+
+        list.subscribe(s -> log.info("{}", s));
+
+//        Thread.sleep(1000);
+
+        StepVerifier.create(list)
+                .expectSubscription()
+                .thenConsumeWhile(l -> {
+                    Assertions.assertFalse(l.isEmpty());
+                    log.info("Size {}", l.size());
+                    return true;
+                })
                 .verifyComplete();
     }
 }
