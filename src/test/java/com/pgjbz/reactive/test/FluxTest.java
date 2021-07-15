5500706f5155069855684c12f3968b3be1128cb3
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2021-07-15 19:31:08 -0300

    Autoconnect ConnectableFlux

--- src/test/java/com/pgjbz/reactive/test/FluxTest.java
+++ src/test/java/com/pgjbz/reactive/test/FluxTest.java
@@ -227,4 +227,22 @@ class FluxTest {
 
     }
 
+    @Test
+    void connectableFluxAutoConnect() throws Exception {
+        Flux<Integer> fluxAutoConnect = Flux.range(1, 5)
+                .log()
+                .delayElements(Duration.ofMillis(100))
+                .publish()
+                .autoConnect(2);
+
+
+        StepVerifier
+                .create(fluxAutoConnect)
+                .then(fluxAutoConnect::subscribe)
+                .expectNext(1,2,3,4,5)
+                .expectComplete()
+                .verify();
+
+    }
+
 }
