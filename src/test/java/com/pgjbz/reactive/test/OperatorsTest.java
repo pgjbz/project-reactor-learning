eed2ef7570bd0c28b9f4710106752d0e82daf600
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2021-07-20 21:21:43 -0300

    Avoiding blocking calls with Blockhound

--- src/test/java/com/pgjbz/reactive/test/OperatorsTest.java
+++ src/test/java/com/pgjbz/reactive/test/OperatorsTest.java
@@ -6,7 +6,10 @@ import lombok.Getter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import reactor.blockhound.BlockHound;
+import reactor.blockhound.BlockingOperationError;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -16,10 +19,36 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 class OperatorsTest {
+
+    @BeforeAll
+    static void setup(){
+        BlockHound.install(builder -> {
+            builder.allowBlockingCallsInside("org.slf4j.impl.SimpleLogger", "write"); //allow blocking
+        });
+    }
+
+    @Test
+    void blockHoundWorks(){
+        try {
+            FutureTask<?> task = new FutureTask<>(() -> {
+                Thread.sleep(0);
+                return "";
+            });
+
+            Schedulers.parallel().schedule(task);
+
+            task.get(10, TimeUnit.SECONDS);
+            Assertions.fail("should fail");
+        } catch (Exception e) {
+            Assertions.assertTrue(e.getCause() instanceof BlockingOperationError);
+        }
+    }
 
     @Test
     void subscribeOnSimple() {
